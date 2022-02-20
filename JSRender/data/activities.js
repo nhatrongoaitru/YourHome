0b39445cceb41180108286860637af5a582fa80c
LeHaiDangSPKT <88248896+LeHaiDangSPKT@users.noreply.github.com>
2022-02-20 16:24:34 +0700

    Update Activities

--- JSRender/data/activities.js
+++ JSRender/data/activities.js
@@ -4,12 +4,17 @@ function activities() {
         {
             img: "./img/Activities/dailyEng.jpg",
             content: "Tiếng Anh Mỗi Ngày",
-            link: ""
+            link: "https://www.facebook.com/hashtag/dailyenglish"
         },
         {
             img: "./img/Activities/XTN.jpg",
             content: "Xuân Tình Nguyện 2022",
-            link: ""
+            link: "https://www.facebook.com/hashtag/bpthsv_lchsvnt"
+        },
+        {
+            img: "./img/Activities/XTN.jpg",
+            content: "Radio Hành Trình Yêu Thương",
+            link: "https://www.facebook.com/hashtag/radiohanhtrinhyeuthuong"
         },
        
     ]
--- JSRender/data/table.js
+++ JSRender/data/table.js
@@ -2,24 +2,24 @@ function table() {
     return table =
     [
         {
-            name: "Hoạt động thứ nhất",
+            name: "Xuân tình nguyện",
             state: "Đã diễn ra"
         },
         {
-            name: "Hoạt động thứ nhất",
-            state: "Đã diễn ra"
+            name: "Tiếng anh mỗi ngày",
+            state: "Đang diễn ra"
         },
         {
-            name: "Hoạt động thứ nhất",
-            state: "Đã diễn ra"
+            name: "Radio hành trình yêu thương",
+            state: "Đang diễn ra"
         },
         {
-            name: "Hoạt động thứ nhất",
-            state: "Đã diễn ra"
+            name: "Find your home",
+            state: "Đang diễn ra"
         },
         {
-            name: "Hoạt động thứ nhất",
-            state: "Đã diễn ra"
+            name: "Giải bóng đá đồng hương HCMUTE",
+            state: "Sắp diễn ra"
         },
         
     ]
